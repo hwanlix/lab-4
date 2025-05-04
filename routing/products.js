1ddead2da8154641e73aa52526c9d8c8d63bcb69
kurloal <kurloal_aehit@students.vizja.pl>
2025-05-04 19:40:26 +0300

    added lab-4

---
+++ routing/products.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const productsController = require('../controllers/productsController');
+
+router.get("/", productsController.getProductsView);
+
+router.get("/add", productsController.getAddProductView);
+
+router.post("/add", productsController.addNewProduct);
+
+router.get("/new", productsController.getNewProductView);
+
+router.get('/:name', productsController.getProductView);
+
+router.delete('/:name', productsController.deleteProduct);
+
+module.exports = router;
